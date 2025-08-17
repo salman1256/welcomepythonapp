9d98edfafda9b6f7c13eaf2a658a6deafae5a3ff
salman1256 <salman1256@yahoo.com>
2025-08-17 19:19:32 +0530

    First Change In Code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a + b
+def multi(a, b):
+    return a * b
 
 if __name__ == "__main__":
     print("Welcome to Python App!!!!")
+    print('First Change in Code!!!')
     print("10 + 3 =", add(10, 3))
+    print ("10*3 =",multi(10,3))
--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
 from app import add
+from app import multi
 
 def test_add():
     assert add(2, 3) == 5
+
+def test_mulit():
+    assert multi(2, 3) == 6
